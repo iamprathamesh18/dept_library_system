7d01f12a90ffff8643d04f6c0a58ab7655c48e03
Prathamesh Shinde <121452545+iamprathamesh18@users.noreply.github.com>
2024-07-19 19:47:36 +0530

    Update update.php

--- update.php
+++ update.php
@@ -1,15 +1,15 @@
 <?php
-	session_start(); // Start the session to access session variables
+	session_start(); 
 	$connection = mysqli_connect("localhost","root","");
 	$db = mysqli_select_db($connection,"lms");
-	// Assuming you have a session variable 'user_id' storing the ID of the logged-in user
+	
 	$id = $_SESSION['id'];
-	// Escape user inputs to prevent SQL injection
+	
 	$name = mysqli_real_escape_string($connection, $_POST['name']);
 	$email = mysqli_real_escape_string($connection, $_POST['email']);
 	$mobile = mysqli_real_escape_string($connection, $_POST['mobile']);
 	$address = mysqli_real_escape_string($connection, $_POST['address']);
-	// Update query with WHERE clause to only update the record of the logged-in user
+	
 	$query = "UPDATE users SET name = '$name', email = '$email', mobile = '$mobile', address = '$address' WHERE id = '$id'";
 	$query_run = mysqli_query($connection, $query);
 	if($query_run){
@@ -20,7 +20,7 @@
 	} else {
 		echo '<script type="text/javascript">';
 		echo 'alert("Update failed...");';
-		echo 'window.location.href = "user_dashboard.php";'; // Redirect to dashboard even if update fails
+		echo 'window.location.href = "user_dashboard.php";'; 
 		echo '</script>';
 	}
 ?>
